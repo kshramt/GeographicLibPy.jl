5bdc1d67d7c08e1032b2cc88e2524f8421267e34
kshramt <thisisdummy@example.com>
2016-04-27 13:41:27 +0900

    Less `global` and more robust

--- src/GeographicLibPy.jl
+++ src/GeographicLibPy.jl
@@ -6,10 +6,28 @@ module GeographicLibPy
 import PyCall
 
 
+const geographiclib = PyCall.PyNULL()
+const geographiclib_geodesic = PyCall.PyNULL()
+# const Geodesic = PyCall.PyNULL()
+const WGS84 = PyCall.PyNULL()
+
+
 function __init__()
-    global const geodesic = PyCall.pyimport("geographiclib.geodesic")
-    global const Geodesic = geodesic[:Geodesic]
-    global const WGS84 = Geodesic[:WGS84]
+    try
+        copy!(geographiclib, PyCall.pyimport("geographiclib"))
+    catch e
+        if PyCall.conda
+            info("Installing geographiclib by conda")
+            Conda.add("geographiclib")
+            copy!(geographiclib, PyCall.pyimport("geographiclib"))
+        else
+            error("Failed to import geographiclib Python package: ", e)
+        end
+    end
+
+
+    copy!(geographiclib_geodesic, PyCall.pyimport("geographiclib.geodesic"))
+    copy!(WGS84, geographiclib_geodesic[:Geodesic][:WGS84])
 end
 
 
